9a22b2415b96e565d3242837f74b1f9f27976f22
Joelebwf <joel.e.boeder@wellsfargo.com>
2020-01-06 10:40:03 -0600

    Updated about page to use CSS instead of tables

--- web/src/views/AboutPage.vue
+++ web/src/views/AboutPage.vue
@@ -13,78 +13,96 @@
 -->
 
 <template>
-    <div class="about">
-        <br/>
-        <br/>
-        <center>
-            <p>
+    <div class="about-container">
+            <div class="about-logo">
                 <img src="@/assets/tv.png" alt="OBTV"/>
-            </p>
-            <table width="95%">
-                <tr>
-                    <td>
-                        <p>
-                            Orange Button is an open data exchange standard for the distributed solar PV industry. Comprising
-                            information models, a standard taxonomy, a standard Application Program Interface (API), and supporting
-                            compliance test suites, Orange Button enables the free flow of data between information systems used in
-                            the solar asset lifecycle to decrease long term costs and increase innovation.
-                        </p>
-                        <p>
-                            Orange Button is powered by XBRL. XBRL is a global open data standard that renders information machine-readable
-                            and improves ability to communicate and exchange business information. These communications are defined by
-                            metadata set out in taxonomies, which define individual reporting concepts as well as the relationships
-                            between concepts and other semantic meaning.
-                        </p>
-                        <p>
-                            The Orange Button Taxonomy Viewer allows for searching and displaying Orange Button Entrypoints, Concepts, Types,
-                            and Units by non-XBRL Experts. XBRL Experts may prefer using XBRL desktop software such as
-                            the open source <a href="http://arelle.org/arelle/">Arelle</a> software which fully exposes the
-                            XBRL syntax.
-                        </p>
-                        <p>
-                            The following screens are available in OBTV:
-                        </p>
-                    </td>
-                </tr>
-            </table>
-            <table width="90%">
-                <tr>
-                    <td valign="top"><b>Entrypoints:</b></td>
-                    <td>
-                        A taxonomy entry point identifies a subset (or "view") of a taxonomy. An entry point contains
-                        a set of concepts (concepts may be present in multiple entrypoints) grouped into Abstracts and
-                        Tables.
-                    </td>
-                </tr>
-                <tr>
-                    <td valign="top"><b>Concepts:</b></td>
-                    <td>
-                        A taxonomy element that provides the meaning for a fact. For example, "Profit", "Turnover", and
-                        "Assets" would be typical concepts. Please note that although XBRL contains Abstract Concepts
-                        (concepts that do not have facts) they are not displayed as Concepts in OBTV (use XBRL Desktop
-                        Software to display instead) but rather indirectly reflected in the Entrypoint hierarchies in
-                        order to simplify concept display for business users.
-                    </td>
-                </tr>
-                <tr>
-                    <td valign="top"><b>Types:</b></td>
-                    <td>
-                        Item Types may be a simple data type (String, Integer, ...) but also may also may be more specific
-                        (Monetary, Energy, ...) for various concepts. All concepts have an Item Type.
-                    </td>
-                </tr>
-                <tr>
-                    <td valign="top"><b>Units:</b></td>
-                    <td>Units of measurement may be assigned to certain concepts. Units are required for certain numeric
-                        facts.  For example, monetary units must have an associated currency, such as US dollars (USD).
-                    </td>
-                </tr>
-                <tr>
-                    <td valign="top"><b>Glossary</b></td>
-                    <td>Acronyms and Abbreviations used to shorten Orange Button Concepts.</td>
-                </tr>
-            </table>
-        </center>
-
-    </div>
+            </div>
+            <div class="about-text">
+                <p>
+                    Orange Button is an open data exchange standard for the distributed solar PV industry. Comprising
+                    information models, a standard taxonomy, a standard Application Program Interface (API), and supporting
+                    compliance test suites, Orange Button enables the free flow of data between information systems used in
+                    the solar asset lifecycle to decrease long term costs and increase innovation.
+                </p>
+                <p>
+                    Orange Button is powered by XBRL. XBRL is a global open data standard that renders information machine-readable
+                    and improves ability to communicate and exchange business information. These communications are defined by
+                    metadata set out in taxonomies, which define individual reporting concepts as well as the relationships
+                    between concepts and other semantic meaning.
+                </p>
+                <p>
+                    The Orange Button Taxonomy Viewer allows for searching and displaying Orange Button Entrypoints, Concepts, Types,
+                    and Units by non-XBRL Experts. XBRL Experts may prefer using XBRL desktop software such as
+                    the open source <a href="http://arelle.org/arelle/">Arelle</a> software which fully exposes the
+                    XBRL syntax.
+                </p>
+            </div>
+            <div class="about-descriptions">
+                <p>
+                    <b>The following screens are available in OBTV:</b>
+                </p>
+                <p>
+                    <b>Entrypoints:</b>
+                    A taxonomy entry point identifies a subset (or "view") of a taxonomy. An entry point contains
+                    a set of concepts (concepts may be present in multiple entrypoints) grouped into Abstracts and
+                    Tables.
+                </p>
+                <p>
+                    <b>Concepts:</b>
+                    A taxonomy element that provides the meaning for a fact. For example, "Profit", "Turnover", and
+                    "Assets" would be typical concepts. Please note that although XBRL contains Abstract Concepts
+                    (concepts that do not have facts) they are not displayed as Concepts in OBTV (use XBRL Desktop
+                    Software to display instead) but rather indirectly reflected in the Entrypoint hierarchies in
+                    order to simplify concept display for business users.
+                </p>
+                <p>
+                    <b>Types:</b>
+                    Item Types may be a simple data type (String, Integer, ...) but also may also may be more specific
+                    (Monetary, Energy, ...) for various concepts. All concepts have an Item Type.
+                </p>
+                <p>
+                    <b>Units:</b>
+                    Units of measurement may be assigned to certain concepts. Units are required for certain numeric
+                    facts.  For example, monetary units must have an associated currency, such as US dollars (USD).
+                </p>
+                <p>
+                    <b>Glossary</b>
+                    Acronyms and Abbreviations used to shorten Orange Button Concepts.
+                </p>
+            </div>
+        </div>
 </template>
+
+<style scoped>
+.about-container {
+  margin-top: 20px;
+  display: grid;
+  #grid-template-columns: 0px 250px 15px auto 25px;
+  #grid-template-rows: 250px auto;
+  height: 75%;
+  width: 90%;
+  margin: auto;
+}
+
+.about-logo {
+  grid-column: 1 / 1;
+  grid-row: 1 / 1;
+  width: 50%;
+  margin: auto;
+}
+
+.about-text {
+  grid-column: 1 / 1;
+  grid-row: 2 / 2;
+}
+
+.about-descriptions {
+  grid-column: 1 / 1;
+  grid-row: 3 / 3;
+}
+
+a {
+  color: #4285f4;
+}
+
+</style>
